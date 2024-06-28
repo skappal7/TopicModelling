77cc3d10041636cf912d83fbe0f63c1e1832b085
Sunil Kappal <skappal7@users.noreply.github.com>
2024-06-28 18:30:01 +0530

    Update topicmodel.py

--- topicmodel.py
+++ topicmodel.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import pyLDAvis
-import pyLDAvis.gensim_models
 import tempfile
 import base64
 
@@ -50,13 +49,19 @@ if uploaded_file is not None:
 
         # PyLDAvis visualization
         st.header("PyLDAvis Visualization:")
-        
+
         # Prepare the data for PyLDAvis
-        data = pyLDAvis.prepare(lda, X, vectorizer, sort_topics=False)
-        
+        prepared_data = pyLDAvis.prepare(
+            topic_term_dists=lda.components_,
+            doc_topic_dists=lda_output,
+            doc_lengths=[len(doc) for doc in df[text_column_name]],
+            vocab=vectorizer.get_feature_names_out(),
+            term_frequency=X.toarray().sum(axis=0)
+        )
+
         # Save the visualization to a temporary HTML file
         with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as tmpfile:
-            pyLDAvis.save_html(data, tmpfile.name)
+            pyLDAvis.save_html(prepared_data, tmpfile.name)
             
             # Read the HTML file and encode it
             with open(tmpfile.name, 'rb') as f:
